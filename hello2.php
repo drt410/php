c212ef92d230edfd5947aee1bc80530a1aedd2b1
Nicolas <nicolas.tran410@gmail.com>
2022-05-16 14:06:16 +0200

    lol

---
+++ hello2.php
@@ -0,0 +1,15 @@
+<?php
+ $chaine= 'Bonjour je suis une chaîne générée par<strong> php<strong>';
+ ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p><?php echo $chaine ; ?></p> 
+</body>
+</html>
